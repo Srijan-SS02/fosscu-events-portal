21c347874af8846b7481c2850a36b86056633524
Srijan-SS02 <codeofficialsrijansriv@gmail.com>
2023-02-07 15:57:19 +0530

    Initial structure of models

--- fosscuEvents/Aron/models.py
+++ fosscuEvents/Aron/models.py
@@ -1,22 +1,32 @@
 from django.db import models
 
 # Create your models here.
-
-    
-    
 class Core(models.Model):
     first_name=models.CharField(max_length=200, null=True)
     last_name=models.CharField(max_length=200, null=True)
     email=models.CharField(max_length=200, null=True)
     phone=models.CharField(max_length=200, null=True)
     
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
     
+
 class User(models.Model):
     name=models.CharField(max_length=200, null=True)
     email=models.CharField(max_length=200, null=True)
     phone=models.CharField(max_length=200, null=True)
 
+    def __str__(self) -> str:
+        return self.name
 
 
-# class Events(models.Model):
-#     ...
+class Event(models.Model):
+    name=models.CharField(max_length=200, null=True)
+    Date=models.DateTimeField(auto_now_add=True, null=True)
+    Description=models.CharField(max_length=1000, null=True)
+    Attendee=models.IntegerField(blank=True)
+    eventImage=models.ImageField(default="FOSSCUlogo.png", blank=True, null=True)
+    
+    
+    def __str__(self) -> str:
+        return self.name
